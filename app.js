22980fcca8f15dd55cb16a031e10951b4d95d509
hasodev <158588225+hasodev@users.noreply.github.com>
2024-02-02 08:37:16 -0600

    initial beta

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del Número Secreto';
+
+let parrafo = document.querySelector('p');
+    parrafo.innerHTML = 'Indica un número entre 1 y 10';
